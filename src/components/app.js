1858cbed15a0f8cffb35b49d8b5aa7fccd752a8a
Logesh Thulasiraman <logesht@zoho.com>
2018-06-05 09:30:56 +0530

    made some changes

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Login from '../containers/login'
-import { Route } from 'react-router-dom'
+import { Route, Redirect } from 'react-router-dom'
 
 const App = () => (
   <div>
     <main>
+      <Redirect from="/" to="login" />
       <Route path="/login" component={Login} />
       <Route path="/abt" render={() => <div>This works baby!</div>} />
     </main>
--- src/containers/login.js
+++ src/containers/login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import { login } from '../actions/login';
 import { loginFailure } from '../actions/login';
@@ -29,53 +29,61 @@ const mapDispatchToProps = dispatch => ({
     },
     handleLogout:() => {
         dispatch(logout());
-    },
+    }
 })
-  
-const Login = ({ user, responseFB, handleRedirection, handleLogout}) => {
-    if(!user.name || user.show_error_message){
+
+class Login extends Component{
+    RenderLoginView() {
         return (
             <MuiThemeProvider style={center_container_style}>
-            <div style={center_container_style}>
-                <TextField
-                    hintText="Enter your Username"
-                    floatingLabelText="Username"
-                />
-                <br />
-                <TextField
-                    type="password"
-                    hintText="Enter your Password"
-                    floatingLabelText="Password"
-                />
-                <br />
-                <RaisedButton label="Submit" primary={true} onClick={(event) => this.props.handleOnSubmit()}/>
-                <br />
-                <div>
-                    <FacebookLogin
-                    appId="165378730824562"
-                    fields="name,email,picture"
-                    callback={responseFB} />
+                <div style={center_container_style}>
+                    <TextField
+                        hintText="Enter your Username"
+                        floatingLabelText="Username"
+                    />
+                    <br />
+                    <TextField
+                        type="password"
+                        hintText="Enter your Password"
+                        floatingLabelText="Password"
+                    />
+                    <br />
+                    <RaisedButton label="Submit" primary={true} onClick={(event) => this.props.handleOnSubmit()}/>
+                    <br />
+                    <div>
+                        <FacebookLogin
+                        appId="165378730824562"
+                        fields="name,email,picture"
+                        callback={this.props.responseFB} />
+                    </div>
+                    <br />
+                    {this.props.user.show_error_message ? <div style={error_message}>Try logging in again!</div> : null}
                 </div>
-                <br />
-                {user.show_error_message ? <div style={error_message}>Try logging in again!</div> : null}
-            </div>
             </MuiThemeProvider>
         )
     }
-    else{
+    RenderUserView() {
       return(
         <MuiThemeProvider style={center_container_style}>
             <div style={center_container_style}>
-                <Avatar facebookId={user.user_id} size={250} round={true}/>
+                <Avatar facebookId={this.props.user.user_id} size={250} round={true}/>
                 <br />
-                <Link to="/abt"><RaisedButton label={'Continue as ' + user.name} primary={true}/></Link>
+                <Link to="/abt"><RaisedButton label={'Continue as ' + this.props.user.name} primary={true}/></Link>
                 <br />
-                <Link to="/login"><RaisedButton label={'Logout'} primary={true} onClick={() => handleLogout()}/></Link>
+                <Link to="/login"><RaisedButton label={'Logout'} primary={true} onClick={() => this.props.handleLogout()}/></Link>
             </div>
         </MuiThemeProvider>
       )
     }
-}
+    render(){
+        if(!this.props.user.name || this.props.user.show_error_message){
+            return this.RenderLoginView()
+        }
+        else{
+            return this.RenderUserView()
+        }
+    }
+} 
 
 const center_container_style = {
     display: 'flex', 
